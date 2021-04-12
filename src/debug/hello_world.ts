7dfa41cbc90b614b5ea8ce18325d41b2384f6bc3
devin7788 <andy129408@gmail.com>
2021-04-12 14:53:26 +0800

    add modules

--- src/debug/hello_world.ts
+++ src/debug/hello_world.ts
@@ -1,5 +1,8 @@
 // npm install --save-dev typescript ts-node
 // -r ts-node/register
+// node parameters: -r ts-node/register
+// working direct: E:\program\node.learn
+// javascript file: $FilePathRelativeToProjectRoot$
 var a : string = "hello world"
 console.log(a)
 let helloWorld = "Hello World";
---
+++ src/orm/orm.ts
@@ -0,0 +1,10 @@
+// npm install typeorm --save
+// npm install reflect-metadata --save
+// npm install @types/node --save-dev
+// npm install mysql --save
+// npm install typeorm -g
+// typeorm init --name MyProject --database mysql/
+// cd MyProject
+// npm install
+// npm start 启动程序
+// 记得创建test数据库
